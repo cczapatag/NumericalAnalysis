9565e55eb9f54e41996975bc78dd46f2af31e15c
cczapatag <cczapatag@eafit.edu.co>
2023-05-26 21:52:47 -0500

    modified:   application/MultipleRoots.py 	modified:   application/NewtonRaphson.py 	modified:   application/Secante.py 	new file:   application/__pycache__/Biseccion.cpython-311.pyc 	new file:   application/__pycache__/Gauss.cpython-311.pyc 	new file:   application/__pycache__/Jacobi.cpython-311.pyc 	new file:   application/__pycache__/MultipleRoots.cpython-311.pyc 	new file:   application/__pycache__/NewtonRaphson.cpython-311.pyc 	new file:   application/__pycache__/PuntoFijo.cpython-311.pyc 	new file:   application/__pycache__/ReglaFalsa.cpython-311.pyc 	new file:   application/__pycache__/Secante.cpython-311.pyc 	new file:   application/__pycache__/__init__.cpython-311.pyc 	new file:   application/__pycache__/admin.cpython-311.pyc 	new file:   application/__pycache__/apps.cpython-311.pyc 	new file:   application/__pycache__/models.cpython-311.pyc 	new file:   application/__pycache__/sor.cpython-311.pyc 	new file:   application/__pycache__/views.cpython-311.pyc 	new file:   application/migrations/__pycache__/__init__.cpython-311.pyc 	modified:   application/templates/multiple-roots.html 	modified:   application/templates/newton-raphson.html 	modified:   application/templates/secant.html 	modified:   application/views.py 	new file:   corolla/__pycache__/__init__.cpython-311.pyc 	new file:   corolla/__pycache__/settings.cpython-311.pyc 	new file:   corolla/__pycache__/urls.cpython-311.pyc 	new file:   corolla/__pycache__/wsgi.cpython-311.pyc

--- application/views.py
+++ application/views.py
@@ -5,7 +5,9 @@ from application.PuntoFijo import PuntoFijo
 from application.Jacobi import Jacobi
 from application.Gauss import Seidel
 from application.sor import SOR
-
+from application.MultipleRoots import MultipleRoots as mr
+from application.NewtonRaphson import NewtonRaphson
+from application.Secante import Secante
 # Create your views here.
 
 def home(request):
@@ -197,13 +199,87 @@ def jacobi(request):
     return render(request, 'jacobi.html', {'resultado': resultado, 'mensaje': mensaje, 'datos': datos})
 
 def multiple_roots(request):
-    return render(request, 'multiple-roots.html')
+    resultado = mensaje = datos = None
+
+    if request.POST:
+        funcion = request.POST['funcion']
+        dfuncion = request.POST['dfuncion']
+        d2funcion = request.POST['d2funcion']
+        x0 = request.POST['x0']
+        tolerancia = request.POST['tolerancia']
+        iteraciones = request.POST['iteraciones']
+
+        x0 = float(x0)
+        tolerancia = float(tolerancia)
+        iteraciones = int(iteraciones)
+
+        datos = {
+            'fun': funcion,
+            'dfun': dfuncion,
+            'd2fun': d2funcion,
+            'x0': x0,
+            'tol': '{:.1e}'.format(tolerancia).replace('e-0', 'e-'),
+            'iter': iteraciones
+        }
+
+        resultado, mensaje = mr.multiple_roots(funcion, dfuncion, d2funcion, x0, tolerancia, iteraciones)
+
+
+    return render(request, 'multiple-roots.html', {'resultado': resultado, 'mensaje': mensaje, 'datos': datos})
 
 def newton(request):
-    return render(request, 'newton-raphson.html')
+
+    result = message = data = None
+
+    if request.POST:
+        functionf = request.POST['functionf']
+        functiondf = request.POST['functiondf'] 
+        x0 = request.POST['x0']
+        tolerance = request.POST['tolerance']
+        iterations = request.POST['iterations']
+
+        x0 = float(x0)
+        tolerance = float(tolerance)
+        iterations = int(iterations)
+
+        data = {
+            'funf' : functionf,
+            'fundf' : functiondf,
+            'x0' : x0,
+            'tol' : '{:.1e}'.format(tolerance).replace('e-0', 'e-'),
+            'iter' : iterations
+        }
+        result, message = NewtonRaphson.newton_raphson(functionf, functiondf, x0, tolerance, iterations)
+
+    return render(request, 'newton-raphson.html', {'result': result, 'message': message, 'data': data})
 
 def secant(request):
-    return render(request, 'secant.html')
+    result = message = data = None
+
+    if request.POST:
+        functionf = request.POST['functionf']
+        x0 = request.POST['x0']
+        x1 = request.POST['x1']
+        tolerance = request.POST['tolerance']
+        iterations = request.POST['iterations']
+
+        x0 = float(x0)
+        x1 = float(x1)
+        tolerance = float(tolerance)
+        iterations = int(iterations)
+
+        data = {
+            'funf' : functionf,
+            'x0' : x0,
+            'x1' : x1,
+            'tol' : '{:.1e}'.format(tolerance).replace('e-0', 'e-'),
+            'iter' : iterations
+        }
+
+        result, message = Secante.secante(functionf, x0, x1, tolerance, iterations)
+
+    return render(request, 'secant.html', {'result': result, 'message': message, 'data': data})
+
 
 def sor(request):
             
